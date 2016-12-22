83dbe9aeaacc91a1f4180424bef45ae577af3e6c
godza-kauyani <kauyanigodfrey@gmail.com>
2016-12-22 18:29:13 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports ={
-	consumer_key : '7pXAXcFA8Z7yAQIe1sGtECTft' ,
-	consumer_secret:'GA54N7YdEbBdeEi02XFlWhcKor55jv3vLOZG1pEvk4m6UUce3X' , 
-	access_token : ' 811914511871606788-8RcUEIzh5bh8VvhwdVGVcpimMRdCORh' , 
-	access_token_secret:'YcPIe7EpGD7m9G8tPTFdOMeYLUU8hdVJCCK9WnVUQTlUv' 
-}
